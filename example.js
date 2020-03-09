ef74a0fc66b3e6b0b613a01a8c4fb2e90ac79b79
LuKks <lucasbarrena@hotmail.com>
2020-03-09 17:38:06 -0300

    removed useless commented code

--- example.js
+++ example.js
@@ -52,37 +52,3 @@ examples.push((async () => {
 
 	app.quit();
 })();
-
-/* Test example
-const {app} = require('electron');
-const t = require('tap');
-const fetch = require('..');
-
-app.on('window-all-closed', () => {});
-
-(async () => {
-	const response = await fetch('https://api.ipify.org');
-
-	t.equal(response.type, 'cors');
-	t.equal(response.url, 'https://api.ipify.org/');
-	t.equal(response.redirected, false);
-	t.equal(response.status, 200);
-	t.equal(response.ok, true);
-	t.equal(response.statusText, 'OK');
-	t.equal(response.bodyUsed, false);
-	const ip = await response.text();
-	t.equal(response.bodyUsed, true);
-	t.equal(ip.length > 6, true);
-
-	const expectedHeaders = [['content-type', 'text/plain']];
-	let i = 0;
-	for (const [key, value] of response.headers.entries()) {
-		t.equal(key, expectedHeaders[i][0]);
-		t.equal(value, expectedHeaders[i][1]);
-		i++;
-	}
-
-	app.quit();
-	t.pass('');
-})();
-*/
